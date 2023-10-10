941b011a49887199bec693d8578c0a2d5ba9b19f
a-bhish3k <abhishekumar692@gmail.com>
2023-10-10 11:11:51 +0530

    initial commit for portfolio

---
+++ main.js
@@ -0,0 +1,83 @@
+// splide carousel
+
+let splide = new Splide(".splide", {
+  type: "loop",
+  perPage: 3,
+  focus: "center",
+  updateOnMove: true,
+  padding: "2%",
+  breakpoints: {
+    1280: {
+      perPage: 1,
+      padding: "30%",
+    },
+    678: {
+      padding: "18%",
+    },
+  },
+});
+
+splide.mount();
+
+// progress bar
+
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// header section
+
+function moveToHome() {
+  const home = document.getElementById("home");
+  home.scrollIntoView({ behavior: "smooth" });
+}
+
+function moveToAbout() {
+  const about = document.getElementById("about");
+  about.scrollIntoView({ behavior: "smooth" });
+}
+
+function moveToWork() {
+  const work = document.getElementById("work");
+  work.scrollIntoView({ behavior: "smooth" });
+}
+
+function moveToContact() {
+  const contact = document.getElementById("contact");
+  contact.scrollIntoView({ behavior: "smooth" });
+}
+
+let hamMenu = document.querySelector(".nav-list");
+let menuIcon = document.querySelector(".ham-menu");
+
+function sideMenu() {
+  hamMenu.classList.toggle("menu-toggle");
+  menuIcon.classList.toggle("ham-menu-close");
+}
+
+// form section
+
+function showForm() {
+  document.getElementById("formShow").style.visibility = "visible";
+  document.body.style.overflowY = "hidden";
+}
+
+function closeForm() {
+  document.getElementById("formShow").style.visibility = "hidden";
+  document.body.style.overflowY = "scroll";
+}
+
+// dark mode
+
+function darkMode() {
+  document.body.classList.toggle("light-mode");
+}
